38305ca66c3fde0ba0bb06ee71678a97a33b12d8
Evan Taylor <evantaylor667@gmail.com>
2019-12-12 16:33:01 -0700

    working in CSS

--- exercises/go-speed-clicker/app.js
+++ exercises/go-speed-clicker/app.js
@@ -1,17 +1,37 @@
+// countdown timer
+// const timer = document.getElementById("timer")
+// let count = 5
+// let intervalId
+// timer.textContent = count
+
+// const countdownTimer = () => {
+//     count--
+//     timer.textContent = count
+// }
+
+// window.addEventListener("load", () => {
+//     intervalId = setInterval(countdownTimer, 1000)
+// })
+// if(intervalId === 5000){
+//     clearInterval(intervalId)
+//     count = 0
+// }
+// NEEDS WORK
+
+// click tracker
 let clicks = localStorage.getItem("clicks") || 0
 const tracker = document.getElementById("tracker")
 
-window.addEventListener("click", function(){
+window.addEventListener("click", () => {
     clicks++
     tracker.textContent = clicks
     localStorage.setItem("clicks", clicks)
+    if(count === 0){
+        clicks = tracker.textContent
+    }
 })
 
 const button = document.getElementById("reset")
-button.addEventListener("click", function(){
-    clicks = -1
-})
+button.addEventListener("click", () => clicks = -1)
 
 tracker.textContent = clicks
-
-// (to do) extra credit = countdown timer
