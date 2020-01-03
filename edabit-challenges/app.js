e26be848c10c2856ee6abbea96b589ec0d25476e
Evan Taylor <evantaylor667@gmail.com>
2020-01-02 17:00:07 -0700

    edabit challenges

--- edabit-challenges/app.js
+++ edabit-challenges/app.js
@@ -37,8 +37,28 @@ function factorial(z) {
 		return x
 	}
 }
-console.log(factorial(4)) // --> 24
-console.log(factorial(0)) // --> 1
-console.log(factorial(9)) // --> 362880
-console.log(factorial(1)) // --> 1
-console.log(factorial(2)) // --> 2
+// console.log(factorial(4)) // --> 24
+// console.log(factorial(0)) // --> 1
+// console.log(factorial(9)) // --> 362880
+// console.log(factorial(1)) // --> 1
+// console.log(factorial(2)) // --> 2
+
+
+// 1/2/20
+// Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element:
+const getAbsSum = arr => {
+	let sum = 0
+	for(i = 0; i < arr.length; i++){
+		if(arr[i] < 0){
+			sum -= arr[i]
+		} else {
+			sum += arr[i]
+		}
+	}
+	return sum
+}
+console.log(getAbsSum([2, -1, -3, 4, 8])) // -> 18
+console.log(getAbsSum([-1])) // -> 1
+console.log(getAbsSum([-1, -3, -5, -4, -10, 0])) // -> 23
+console.log(getAbsSum([8, 9, 10, 32, 101, -10])) // -> 170
+console.log(getAbsSum([500])) // -> 500
