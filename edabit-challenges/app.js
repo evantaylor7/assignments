47ece00e91ab64acbf60e2ac53d5161c8a8746ae
Evan Taylor <evantaylor667@gmail.com>
2020-02-07 17:40:12 -0700

    edabit challenge

--- edabit-challenges/app.js
+++ edabit-challenges/app.js
@@ -306,6 +306,25 @@ const greaterThanOne = frac => eval(frac) > 1
 // could have also been:
 const getSumOfItems = arr => arr.reduce((a, b) => a + b)
 
-console.log(getSumOfItems([2, 7, 4])) // -> 13
-console.log(getSumOfItems([45, 3, 0])) // -> 48
-console.log(getSumOfItems([-2, 84, 23])) // -> 105
+// console.log(getSumOfItems([2, 7, 4])) // -> 13
+// console.log(getSumOfItems([45, 3, 0])) // -> 48
+// console.log(getSumOfItems([-2, 84, 23])) // -> 105
+
+
+// 2/7/20
+// Create the function that takes an array with objects and returns the sum of people's budgets.
+// function getBudgets(arr) {
+// 	let count = 0
+// 	for(let i = 0; i < arr.length; i++){
+// 		count += arr[i].budget
+// 	}
+// 	return count
+// }
+
+// using reduce:
+const getBudgets = arr => arr.reduce((a, b) => a + b.budget, 0)
+
+console.log(getBudgets([{name: "John",  age: 21, budget: 23000}, {name: "Steve",  age: 32, budget: 40000}, {name: "Martin",  age: 16, budget: 2700}])) // -> 65700
+console.log(getBudgets([{name: "John",  age: 21, budget: 29000}, {name: "Steve",  age: 32, budget: 32000}, {name: "Martin",  age: 16, budget: 1600}])) // -> 62600
+console.log(getBudgets([{name: "John",  age: 21, budget: 19401}, {name: "Steve",  age: 32, budget: 12321}, {name: "Martin",  age: 16, budget: 1204}])) // -> 32926
+console.log(getBudgets([{name: "John",  age: 21, budget: 10234}, {name: "Steve",  age: 32, budget: 21754}, {name: "Martin",  age: 16, budget: 4935}])) // -> 36923
