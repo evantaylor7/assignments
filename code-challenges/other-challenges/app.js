84788dc3a58ea9c39a2a8bf289fb0700b5b70fe3
Evan Taylor <evantaylor667@gmail.com>
2020-02-27 10:31:26 -0700

    coding challenge

--- code-challenges/other-challenges/app.js
+++ code-challenges/other-challenges/app.js
@@ -9,6 +9,7 @@ const stringToObject = str => {
 
 }
 
+
 // 2/25/20
 // given an array of strings and a string, count how many instances of that string are in the array
 // countStrings(["cat", "cow", "cat"], "cat") => 2
@@ -19,5 +20,24 @@ const stringToObject = str => {
 
 const countStrings = (array, string) => array.filter(word => word === string).length
 
-console.log(countStrings(["cat", "cow", "cat"], "cat")) // -> 2
-console.log(countStrings(["no", "yes", "yes", "yes", "no"], "yes")) // -> 3
+// console.log(countStrings(["cat", "cow", "cat"], "cat")) // -> 2
+// console.log(countStrings(["no", "yes", "yes", "yes", "no"], "yes")) // -> 3
+
+
+// 2/27/20
+// Write a function that takes an unsorted array as a parameter, and returns the missing number.
+// The missing number is the one missing from the array of numbers if the numbers were sorted from least to greatest.
+
+function findMissing(arr){
+    const sortedArr = arr.sort((a, b) => a - b)
+    let missingNum
+    for(let i = 0; i < sortedArr.length; i++){
+        if((sortedArr[i + 1] - sortedArr[i]) > 1){
+            missingNum = sortedArr[i] + 1
+        }
+    }
+    return missingNum
+}
+
+ console.log(findMissing([3, 5, 4, 8, 1, 2, 7])) // 6
+ console.log(findMissing([10, 14, 12, 11, 16, 18, 17, 13])) // 15
