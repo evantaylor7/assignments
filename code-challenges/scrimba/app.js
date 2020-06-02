14a38e2e40ea4706c117b7e48082ecd4b62c6b3b
Evan Taylor <evantaylor667@gmail.com>
2020-06-02 12:20:59 -0600

    scrimba coding challenge

--- code-challenges/scrimba/app.js
+++ code-challenges/scrimba/app.js
@@ -1,5 +1,5 @@
 // Scrimba's one week of JavaScript challenges!
-// DAY 1
+// DAY 1:
 
 // add a border of * around entire matrix of strings like so:
 // [
@@ -24,4 +24,22 @@ function addBorder(array) {
 
 const array = ['abc', 'ded']
 
-console.log(addBorder(array))
+// console.log(addBorder(array))
+
+
+// DAY 2:
+
+// take a two (or more) digit number and return the sum of their numbers
+function addTwoDigits(num)  {
+    const arr = num.toString().split('')
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+        sum += Number(arr[i])
+    }
+    return sum
+}
+
+console.log(addTwoDigits(29)) // --> 11
+console.log(addTwoDigits(295)) // --> 16
+console.log(addTwoDigits(85)) // --> 13
+console.log(addTwoDigits(4758992)) // --> 44
